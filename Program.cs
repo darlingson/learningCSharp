e59f3c5cddc9041ab1efb112cf23d2830254ecdc
darlingson <dmakuwila@gmail.com>
2023-11-18 12:26:25 +0200

    drew a triagle shape

--- Program.cs
+++ Program.cs
@@ -10,8 +10,12 @@ namespace learningCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
-            Console.WriteLine("Hello, World 2");
+            Console.WriteLine("     / |");
+            Console.WriteLine("    /  |");
+            Console.WriteLine("   /   |");
+            Console.WriteLine("  /    |");
+            Console.WriteLine(" /     |");
+            Console.WriteLine("/ _____|");
 
             Console.ReadLine();
         }

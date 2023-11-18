33baf0e9bf113aef14d8ccac6c3326f7dac12393
darlingson <dmakuwila@gmail.com>
2023-11-18 14:26:19 +0200

    number methods

--- Program.cs
+++ Program.cs
@@ -10,21 +10,9 @@ namespace learningCSharp
     {
         static void Main(string[] args)
         {
-            string name = "Darlingson" + " is cool"; //string concatenation
-            Console.WriteLine("Darlingson");
-            Console.WriteLine("Darlingson\nMakuwila"); //printing to new line
-            Console.WriteLine("Darlingson\"Makuwila"); //deleniting a "
-
-            Console.WriteLine(name);// printing a string from variable
-            Console.WriteLine(name.Length); //length of string
-
-            //String methods
-            Console.WriteLine(name.ToUpper()); //uppercase
-            Console.WriteLine(name.ToLower()); //lowercase
-            Console.WriteLine(name.Contains("is cool")); //checking for substring
-            Console.WriteLine(name[0]); // finding the character at index 0
-            Console.WriteLine(name.IndexOf("is")); //find the position of substring
-            Console.WriteLine(name.Substring(11));
+            Console.WriteLine(5+8);// csharp will add the numbers and print the result
+            Console.WriteLine(5 + 9 * 2);// normal order of operation
+            Console.WriteLine((5 + 9) * 2);// specified order of operation
 
             Console.ReadLine();
         }

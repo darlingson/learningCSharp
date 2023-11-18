f31a684157e065bf801d70a6609a50b9d97246f4
darlingson <dmakuwila@gmail.com>
2023-11-18 13:50:30 +0200

    learnt about printing variables

--- Program.cs
+++ Program.cs
@@ -10,12 +10,14 @@ namespace learningCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("     / |");
-            Console.WriteLine("    /  |");
-            Console.WriteLine("   /   |");
-            Console.WriteLine("  /    |");
-            Console.WriteLine(" /     |");
-            Console.WriteLine("/ _____|");
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
 
             Console.ReadLine();
         }

859946c14a62641204c561788975018c2f500427
darlingson <dmakuwila@gmail.com>
2023-11-19 17:38:21 +0200

    if statements part 2

--- Program.cs
+++ Program.cs
@@ -10,27 +10,26 @@ namespace learningCSharp
     {
         static void Main(string[] args)
         {
-            //C# if statements
-            bool isMale = false;
-            bool isTall = false;
+            //C# if statements part 2
 
-            if (isMale && isTall)
+            Console.WriteLine(GetMax(2,5));
+
+            Console.ReadLine();
+        }
+        static int GetMax(int num1, int num2)
+        {
+            int result;
+
+            if (num1 > num2)
             {
-                Console.WriteLine("You are a Tall Male");
-            }else if (isMale && !isTall)
-            {
-                Console.WriteLine("You are a short male");
-            }
-            else if (!isMale && isTall)
-            {
-                Console.WriteLine("You are a tall female");
+                result = num1;
             }
             else
             {
-                Console.WriteLine("You are a short Female");
+                result = num2;
             }
 
-            Console.ReadLine();
+            return result;
         }
     }
 }

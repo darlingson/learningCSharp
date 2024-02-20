34c1701545b87988f96c2dcfc9bd436675fff866
darlingson <dmakuwila@gmail.com>
2024-02-20 07:43:31 +0200

    started the basics of objects and classes

--- Program.cs
+++ Program.cs
@@ -23,7 +23,10 @@ namespace learningCSharp
             //forLoop();
             //exponents();
             //twoDArray();
-            exceptionHandling();
+            //exceptionHandling();
+            objectsandclasses();
+
+
         }
         static int GetMax(int num1, int num2)
         {
@@ -224,6 +227,21 @@ namespace learningCSharp
             {
                 Console.WriteLine("");
             }
+        }
+        static void objectsandclasses()
+        {
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book2.title = "Lord of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book2.pages);
         }
     }
 }
